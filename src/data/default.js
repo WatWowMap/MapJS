2feed229834ddfad90ec76f509379525f35ea46d
versx <versx@ver.sx>
2020-07-19 22:16:51 -0700

    Add cluster option for Pokemon

--- src/data/default.js
+++ src/data/default.js
@@ -8,5 +8,6 @@ data.style = config.style == 'dark' ? 'dark' : '';
 data.max_pokemon_id = config.map.maxPokemonId;
 data.google_analytics_id = config.google.analytics;
 data.google_adsense_id = config.google.adsense;
+data.cluster_pokemon = config.map.clusters.pokemon;
 
 module.exports = data;
