52e50f5e2cacac695c6def124001c79d6e252413
versx <versx@ver.sx>
2020-07-21 11:09:30 -0700

    oops

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ const utils = require('./services/utils.js');
 // TODO: Separate cluster layers by type
 // TODO: Limit number of scouts per user
 // TODO: Query db for quests to shorten filter list
-// TODO: Use dots instead of icons for Spawnpoints
 
 // Basic security protection middleware
 app.use(helmet());
