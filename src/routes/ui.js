e6e5fc93a35e3de8a17373807dc0ff941ace6aa6
versx <versx@ver.sx>
2020-08-16 22:18:52 -0700

    Use cookie sessions

--- src/routes/ui.js
+++ src/routes/ui.js
@@ -16,10 +16,8 @@ if (config.discord.enabled) {
     });
 
     router.get('/logout', (req, res) => {
-        req.session.destroy((err) => {
-            if (err) throw err;
-            res.redirect('/login');
-        });
+        req.session = null;
+        res.redirect('/login');
     });
 }
 
