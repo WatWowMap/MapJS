3adfa05b2aedd96f49b40bea2f52b339ee602e7c
sketchysocks <58572875+sketchysocks@users.noreply.github.com>
2020-08-25 14:13:10 -0400

    Update map.js

--- src/data/map.js
+++ src/data/map.js
@@ -1220,8 +1220,8 @@ const getSearchData = async (lat, lon, id, value) => {
                     if (result.quest_item_id > 0) {
                         result.url2 = config.icons['Default'/* TODO: Add icon style */] + `/item/${result.quest_item_id}.png`;
                     } else if (result.quest_pokemon_id > 0) {
-                        const formId = result.quest_pokemon_form_id ? result.quest_pokemon_form_id : '00';
-                        result.url2 = config.icons['Default'/* TODO: Add icon style */] + `/pokemon/pokemon_icon_${utils.zeroPad(result.quest_pokemon_id, 3)}_${utils.zeroPad(formId, 2)}.png`;
+                        const formId = result.quest_pokemon_form_id > 0 ? result.quest_pokemon_form_id : '00';
+                        result.url2 = config.icons['Default'/* TODO: Add icon style */] + `/pokemon/pokemon_icon_${utils.zeroPad(result.quest_pokemon_id, 3)}_${formId}.png`;
                     } else if (result.quest_reward_type === 3) {
                         result.url2 = config.icons['Default'/* TODO: Add icon style */] + '/item/-1.png';
                     }
