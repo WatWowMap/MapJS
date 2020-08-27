be1648130981792f724f9d331c701e251ab87372
sketchysocks <58572875+sketchysocks@users.noreply.github.com>
2020-08-26 20:59:53 -0400

    Rarity Tiers in Config Now
    - Moved rarity tiers to config to be customizable
    
    - Renamed rarity.json to generation.json, since that's all it is now
    
    - Cleaned up Quick Start code to be all quick start related, not recommended & quick start :joy:
    
    - Changed default IV filters to be 90-100 and PVP 1-3, because users need "help".

--- src/data/default.js
+++ src/data/default.js
@@ -31,6 +31,12 @@ data.default_show_submissioncells = config.map.filters.submissionCells;
 data.default_show_nests = config.map.filters.nests;
 data.default_show_scanareas = config.map.filters.scanAreas;
 data.default_show_devices = config.map.filters.devices;
-data.recommended_pokemon_json = JSON.stringify(config.recommendedPokemon);
+data.quickStart_pokemon_json = JSON.stringify(config.pokemon.quickStart);
+data.common_pokemon_json = JSON.stringify(config.pokemon.common);
+data.uncommon_pokemon_json = JSON.stringify(config.pokemon.uncommon);
+data.rare_pokemon_json = JSON.stringify(config.pokemon.rare);
+data.ultraRare_pokemon_json = JSON.stringify(config.pokemon.ultraRare);
+data.regional_pokemon_json = JSON.stringify(config.pokemon.regional);
+data.event_pokemon_json = JSON.stringify(config.pokemon.event);
 
 module.exports = data;
