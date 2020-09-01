1a73ad602a6cd59c9a5d2af15a02d9008991626a
Mygod <contact-git@mygod.be>
2020-09-01 11:21:39 -0400

    Add new availableItems

--- src/routes/ui.js
+++ src/routes/ui.js
@@ -209,7 +209,7 @@ const handleHomeJs = async (req, res) => {
     data.available_icon_styles_json = JSON.stringify(config.icons);
 
     // Build available items list
-    const availableItems = [-3, -2, -1];
+    const availableItems = [-1, -2, -3, -4, -5, -6, -7, -8];
     //const keys = Object.keys(InventoryItemId);
     //keys.forEach(key => {
     //    const itemId = InventoryItemId[key];
