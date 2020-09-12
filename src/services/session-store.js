267f0ce86f98c2a7054f8673a208c4c79092621b
versx <versx@ver.sx>
2020-09-12 16:44:51 -0700

    Config reference fixes

--- src/services/session-store.js
+++ src/services/session-store.js
@@ -3,7 +3,7 @@
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 
-const config = require('../config.json');
+const config = require('../services/config.js');
 const MySQLConnector = require('../services/mysql.js');
 const db = new MySQLConnector(config.db.scanner);
 
