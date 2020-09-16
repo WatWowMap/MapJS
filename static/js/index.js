27f967f62fa840c908b8c8a8b8406c4c939a9b7a
Mygod <contact-git@mygod.be>
2020-09-16 12:11:52 -0400

    Update IV filter example

--- static/js/index.js
+++ static/js/index.js
@@ -5153,7 +5153,7 @@ function getTimeSince (date) {
     return str;
 }
 
-const ivFilterPrompt = 'Please enter an IV Filter. Example: (S0-1 & A15 & D15 & (C1400-1500 | C2400-2500)) | L35 | 90-100';
+const ivFilterPrompt = 'Please enter an IV Filter. Example: (S0-1 & A15 & D15 & (CP1400-1500 | CP2400-2500)) | L35 | 90-100';
 
 function manageIVPopup (id, filter) {
     const result = prompt(ivFilterPrompt, filter[id].filter).toUpperCase();
