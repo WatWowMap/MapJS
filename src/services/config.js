4fbd4168215eccbdca3acb302c7b47ee403088e8
PartTimeJS <r.g.levasseur@gmail.com>
2020-10-26 10:20:05 -0400

    Update config.js
    If another config file is added, it would not merge properly.

--- src/services/config.js
+++ src/services/config.js
@@ -2,14 +2,13 @@
 
 const extend = require('extend');
 const uConfig = require('../configs/config.json');
-const eConfig = require('../configs/default.json');
-const target = {};
+const dConfig = require('../configs/default.json');
 /* 
  * deep Boolean (optional) If set, the merge becomes recursive (i.e. deep copy).
  * target Object The object to extend.
  * object1 Object The object that will be merged into the first.
  * objectN Object (Optional) More objects to merge into the first.
 */
-extend(true, target, eConfig, uConfig);
+extend(true, dConfig, uConfig);
 
-module.exports = target;
+module.exports = dConfig;
