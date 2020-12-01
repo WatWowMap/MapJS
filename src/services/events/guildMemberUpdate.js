df5fdf0260b15e23484ea728f5fe9e0dc585216f
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2020-11-30 22:39:26 -0500

    Update src/services/events/guildMemberUpdate.js
    Works great!
    
    Co-authored-by: Clayton Burlison <git@clburlison.com>

--- src/services/events/guildMemberUpdate.js
+++ src/services/events/guildMemberUpdate.js
@@ -1,10 +1,22 @@
 const { clearOtherSessions } = require('../session-store.js');
 
 module.exports = async (client, oldPresence, newPresence) => {
-    const roleBefore = oldPresence.roles.cache.find((role) => client.config.discord.perms.map.roles.includes(role.id));
-    const roleAfter = newPresence.roles.cache.find((role) => client.config.discord.perms.map.roles.includes(role.id));
+    const rolesBefore = oldPresence.roles.cache
+                .filter(x => BigInt(x.id).toString())
+                .keyArray();
+    const rolesAfter = newPresence.roles.cache
+                .filter(x => BigInt(x.id).toString())
+                .keyArray();
+    const perms = Object.values(client.config.discord.perms)
+                .map(x => x.roles)
+                .reduce(x => [...x]);
+    const roleDiff = rolesBefore
+                 .filter(x => !rolesAfter.includes(x))
+                 .concat(rolesAfter
+                    .filter(x => !rolesBefore.includes(x))
+                 );
     try {
-        if (roleBefore && !roleAfter) {
+        if (perms.includes(roleDiff[0])) {
             await clearOtherSessions(oldPresence.user.id, '');
         }
     } catch (e) {
