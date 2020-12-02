49f189c3a2fb62572f71eddf9e07bf5be7c7f777
versx <versx@ver.sx>
2020-12-01 22:42:00 -0800

    Fix lint

--- src/services/events/guildMemberUpdate.js
+++ src/services/events/guildMemberUpdate.js
@@ -1,20 +1,21 @@
+/* global BigInt */
 const { clearOtherSessions } = require('../session-store.js');
 
 module.exports = async (client, oldPresence, newPresence) => {
     const rolesBefore = oldPresence.roles.cache
-                .filter(x => BigInt(x.id).toString())
-                .keyArray();
+        .filter(x => BigInt(x.id).toString())
+        .keyArray();
     const rolesAfter = newPresence.roles.cache
-                .filter(x => BigInt(x.id).toString())
-                .keyArray();
+        .filter(x => BigInt(x.id).toString())
+        .keyArray();
     const perms = Object.values(client.config.discord.perms)
-                .map(x => x.roles)
-                .reduce(x => [...x]);
+        .map(x => x.roles)
+        .reduce(x => [...x]);
     const roleDiff = rolesBefore
-                 .filter(x => !rolesAfter.includes(x))
-                 .concat(rolesAfter
-                    .filter(x => !rolesBefore.includes(x))
-                 );
+        .filter(x => !rolesAfter.includes(x))
+        .concat(rolesAfter
+            .filter(x => !rolesBefore.includes(x))
+        );
     try {
         if (perms.includes(roleDiff[0])) {
             await clearOtherSessions(oldPresence.user.id, '');
