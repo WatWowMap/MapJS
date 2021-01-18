fdef08a211c33951280163ec579b339f3221ddc7
versx <versx@ver.sx>
2021-01-18 11:13:33 -0800

    Reference ip variable

--- src/routes/discord.js
+++ src/routes/discord.js
@@ -73,7 +73,7 @@ router.get('/callback', catchAsyncErrors(async (req, res) => {
                 },
                 { 
                     name: 'Ip Address',
-                    value: `||${req.headers['cf-connecting-ip']}||` 
+                    value: `||${ip}||` 
                 },
                 {
                     name: 'Geo Lookup',
